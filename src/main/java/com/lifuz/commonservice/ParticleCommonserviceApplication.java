25f3dd86f2ceb12b57310c6c6583480be3baab37
lifuz <lifuzz@163.com>
2017-11-01 17:33:25 +0800

    Initial commit

---
+++ src/main/java/com/lifuz/commonservice/ParticleCommonserviceApplication.java
@@ -0,0 +1,12 @@
+package com.lifuz.commonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticleCommonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticleCommonserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lifuz/commonservice/ParticleCommonserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lifuz.commonservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParticleCommonserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
